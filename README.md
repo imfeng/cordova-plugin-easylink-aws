3fd78cfbfe616f8d1bfa57817071f589639cd269
HermitF <fantasrulai@gmail.com>
2018-07-06 04:18:23 +0800

    bug fixed, 少lib

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # cordova-plugin-easylink-aws
 "# cordova-plugin-easylink-aws"
+---
 modify from [@GTDistance/cordova-plugin-easylink](https://github.com/GTDistance/cordova-plugin-easylink)
+---
+- 缺少
+    ```
+    compile 'org.eclipse.jetty:jetty-servlet:9.2.0.RC0'
+    compile 'org.eclipse.jetty:jetty-server:9.2.0.RC0'
+    ```
+- 缺少
+    platforms/android/src/
+        io.fogcloud...
+
+
+
+
--- src/android/com/thomas/easylink_tes/EasyLink_tes.java
+++ src/android/com/thomas/easylink_tes/EasyLink_tes.java
@@ -29,51 +29,51 @@ public class EasyLink_tes extends CordovaPlugin{
 
     private Context ctx = null;
     /** 0.2.6 */
-    private EasyLink el;
-    private EasylinkP2P elp2p;
+    public EasyLink el;
+    public EasylinkP2P elp2p;
     /** */
 
     @Override
     protected void pluginInitialize() {
         super.pluginInitialize();
-        ctx = cordova.getActivity();
+        this.ctx = cordova.getActivity();
         /** OLD
         elapi = new EasyLinkAPI(ctx);
         server = new UDPServer();
         */
-        el = new EasyLink(ctx);
-        elp2p = new EasylinkP2P(ctx);
+        this.el = new EasyLink(ctx);
+        this.elp2p = new EasylinkP2P(ctx);
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
+        final CallbackContext _cbc = callbackContext;
         //获取wifiSSid
         if (action.equals("getWifiSSid")){
             /** OLD
             mWifiManager = new EasyLinkWifiManager(ctx);
             callbackContext.success(mWifiManager.getCurrentSSID());
             */
-            callbackContext.success(el.getSSID());
+            _cbc.success(el.getSSID());
             return true;
         }
 
         //开始搜索
         if (action.equals("startSearch")) {
-            startSearch(args,callbackContext);
+            startSearch(args,_cbc);
             return true;
         }
 
         //停止搜索
         if(action.equals("stopSearch")){
-            this.ctxelp2p.stopEasyLink(new EasyLinkCallBack() {
+            this.elp2p.stopEasyLink(new EasyLinkCallBack() {
                 @Override
                 public void onSuccess(int code, String message) {
-                    callbackContext.success(message);
+                    _cbc.success(message);
                 }
                 @Override
                 public void onFailure(int code, String message) {
-                    callbackContext.error(message);
+                    _cbc.error(message);
                 }
             });
             /** OLD
@@ -87,15 +87,16 @@ public class EasyLink_tes extends CordovaPlugin{
     }
 
     private void startSearch(JSONArray args,CallbackContext callbackContext) throws JSONException{
+        final CallbackContext _cbc = callbackContext;
         /** OLD */
         String wifiSSid = args.getString(0).trim();
         String wifiPsw = args.getString(1).trim();
 
         if (wifiSSid==null||wifiSSid.isEmpty()){
-            callbackContext.error("NO SSID");
+            _cbc.error("NO SSID");
 
         }else if (wifiPsw==null||wifiPsw.isEmpty()) {
-            callbackContext.error("NO PASSWORD");
+            _cbc.error("NO PASSWORD");
         } else {
             /** 0.2.6 */
             EasyLinkParams elp = new EasyLinkParams();
@@ -103,15 +104,15 @@ public class EasyLink_tes extends CordovaPlugin{
             elp.password = wifiPsw;
             elp.sleeptime = 50;
             elp.runSecond = 50;
-            elp2p.startEasyLink(elp, new EasyLinkCallBack() {
+            this.elp2p.startEasyLink(elp, new EasyLinkCallBack() {
                 @Override
                 public void onSuccess(int code, String message) {
-                    callbackContext.success(message);
+                    _cbc.success(message);
                 }
 
                 @Override
                 public void onFailure(int code, String message) {
-                    callbackContext.error(message);
+                    _cbc.error(message);
                 }
             });
             /** */
